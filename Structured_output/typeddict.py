1fad912b9ce440e1092c6d3acfc7ae64b13c34ce
Vedshinde06 <vedantshinde501@gmail.com>
2025-08-01 23:21:13 +0530

    structured output using TypedDict

---
+++ Structured_output/typeddict.py
@@ -0,0 +1,42 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated, Literal, Optional
+from pprint import pprint
+
+load_dotenv()
+
+chat = HuggingFaceEndpoint(
+    repo_id= "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm=chat)
+
+#schema
+class Review(TypedDict):
+    key_themes: Annotated[list[str], "Write the key themes from the review in a list"]
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[Literal["pos", "neg"], "Return the senitment of the review"]
+    pros: Annotated[Optional[list[str]], "Return the list of pros if mentioned in the review"]
+    cons: Annotated[Optional[list[str]], "Return the list of cons if mentioned in the review"]
+    name: Annotated[Optional[str], "Name of the reviewer"]
+
+
+structured_model = model.with_structured_output(Review)
+
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful""")
+
+
+print(result['summary'])
+print(result['sentiment'])
+
