cffa22af26dbd99280f2e3ffdaf4112742c514db
Vedshinde06 <vedantshinde501@gmail.com>
2025-08-05 22:15:29 +0530

    added structured output parser in langchain

---
+++ output_parsers/structured_output_parsers.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="google/gemma-2-2b-it", task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+schema = [
+                ResponseSchema(name='fact_1', description='Fact 1 about the tpoic'),
+                ResponseSchema(name='fact_2', description='Fact 2 about the tpoic'),
+                ResponseSchema(name='fact_3', description='Fact 3 about the tpoic')
+
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template = "Wrtie 3 facts about {topic}. \n {format_instructions}",
+    input_variables=['topic'],
+    partial_variables={'format_instructions':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic': 'Black hole'})
+
+print(result)
