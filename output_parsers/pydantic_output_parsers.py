eda8c97aabb7caf0347f60ecd6ff0c614dcf3d8e
Vedshinde06 <vedantshinde501@gmail.com>
2025-08-05 23:06:52 +0530

    added Pydantic output parser implementation

---
+++ output_parsers/pydantic_output_parsers.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="google/gemma-2-2b-it", task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+class Person(BaseModel):
+    
+    name: str = Field(description='name of the person')
+    age: int = Field(ge=0, description='age of the person')
+    city: str = Field(description='City of the person')
+    
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a fictional {place} person. \n {format_instructions}',
+    input_variables=['place'],
+    partial_variables={'format_instructions':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'place':'Maharashtra'})
+
+print(result)
