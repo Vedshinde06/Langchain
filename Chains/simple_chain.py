9727970a5b14b9676171c6568738e12b89fbb7dc
Vedshinde06 <vedantshinde501@gmail.com>
2025-08-11 20:03:25 +0530

    learning langchain component : chains

---
+++ Chains/simple_chain.py
@@ -0,0 +1,23 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm  = HuggingFaceEndpoint(repo_id="openai/gpt-oss-20b", task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template = 'Generate 2 interesting facts about {topic}',
+    input_variables= ['topic']
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser 
+
+result = chain.invoke({'topic' : 'Science'})
+
+print(result)
