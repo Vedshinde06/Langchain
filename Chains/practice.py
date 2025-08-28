f22c0474ef87d2fac21bb54a88c0292274ee1760
Vedshinde06 <vedantshinde501@gmail.com>
2025-08-28 23:34:35 +0530

    langchain runnables

---
+++ Chains/practice.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(model='openai/gpt-oss-20b', task='text-generation')
+
+model = ChatHuggingFace(llm=llm)
+
+
