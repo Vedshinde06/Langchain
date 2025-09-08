ac525561de3efdebae382b1282c6730f9d72ad2d
Vedshinde06 <vedantshinde501@gmail.com>
2025-09-08 23:45:11 +0530

    add text loader for summarizing poems using Hugging Face model

---
+++ Doc_loaders/1.txt_loader.py
@@ -0,0 +1,32 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+model = HuggingFaceEndpoint(repo_id='openai/gpt-oss-20b', task='text-generation')
+
+llm = ChatHuggingFace(llm=model)
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template = 'Write a summary on {poem}',
+    input_variables=['poem']
+)
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].meta_data)
+
+chain = prompt | llm | parser
+
+chain.invoke({'poem':docs[0].page_content})
